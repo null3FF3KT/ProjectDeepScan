ceb478ce56616bd3d5123026796312418171da74
null3FF3KT <cbilledeaux@gmail.com>
2025-02-25 21:07:40 -0600

    This should have been pulled but is being pushed.

--- Services/GitIgnoreParser.cs
+++ Services/GitIgnoreParser.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace ProjectScanner.Services;
+namespace ProjectDeepScan.Services;
 
 public class GitIgnoreParser
 {
